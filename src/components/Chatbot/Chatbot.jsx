633f8f1d7ea2494924f2c1a4242671a7f328d1b9
piotronm <123025664+piotronm@users.noreply.github.com>
2024-04-23 08:38:57 -0400

    Update Chatbot.jsx

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -1,58 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import axios from "axios";
-import "../styles/chatbot.css";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+  List,
+  ListItem,
+  ListItemIcon,
+  Box,
+  Avatar,
+} from "@mui/material";
+import { Done } from "@mui/icons-material";
+import { BeatLoader } from "react-spinners";
 
 const Chatbot = () => {
   const [question, setQuestion] = useState("");
-  const [response, setResponse] = useState("");
+  const [response, setResponse] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [isEmpty, setIsEmpty] = useState(false); // State variable for input validation
-  const [submitted, setSubmitted] = useState(false); // State variable to track form submission
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    console.log("Response:", response);
+  }, [response]);
 
   const handleQuestionSubmit = (event) => {
     event.preventDefault();
     setSubmitted(true);
-
     const query = question.trim();
-
     if (!query) {
       return;
     }
-
     setLoading(true);
 
-    // Use Axios to fetch data from data.json
+    // POST request
+    console.log("Data sent to the server:", { Query: query });
     axios
-      .get("/data2.json")
+      .get("data5.txt", { Query: query })
       .then((response) => {
         const data = response.data;
         console.log("API response received:", data);
-
-        // Process the type and format the content accordingly
-        let formattedContent;
-        if (data.type === "insight") {
-          // Assuming content is JSON, you can parse and format it here
-          formattedContent = data.content;
-        } else {
-          // If type is not 'insight', it's assumed to be 'jira'
-          formattedContent = data.content;
-        }
-
-        // Set the formatted content to state
-        setResponse(formattedContent);
+        setResponse(data);
         setError(null);
       })
       .catch((error) => {
-        console.error("Backup API Error:", error);
+        console.error("API Error:", error);
         setError("Failed to fetch data. Please try again later.");
       })
       .finally(() => {
@@ -61,89 +59,132 @@ const Chatbot = () => {
   };
 
   const handleQuestionChange = (event) => {
-    // Check if the input is empty
     const isEmpty = event.target.value.trim() === "";
-    // Update the isEmpty state
     setIsEmpty(isEmpty);
-    // Use to check if input is being logged
-    // console.log("Input value:", event.target.value);
     setQuestion(event.target.value);
   };
 
-  const CustomTable = ({ data }) => {
-    // Check if data or data.content is undefined or null
-    if (!data || !data.content) {
-      return "No DATA is shown"; // Or render a message indicating no data
+  const renderContent = () => {
+    if (loading) {
+      return null;
     }
 
-    // Get the content object from the data
-    const content = data.content;
+    if (
+      (submitted && !response) ||
+      (response && response.type === "insight" && !response.content)
+    ) {
+      return <p>No data available</p>;
+    }
 
-    // Extract the keys and values from the content object
-    const keys = Object.keys(content);
-    const values = Object.values(content);
+    if (response && response.type === "jira") {
+      return (
+        <div>
+          {submitted && (
+            <ListItem>
+              <ListItemIcon>
+                <Avatar />
+              </ListItemIcon>
+              <span>{question}</span>
+            </ListItem>
+          )}
+          <p>{response.content}</p>
+        </div>
+      );
+    }
 
-    return (
-      <TableContainer component={Paper}>
-        <Table aria-label="Insights table">
-          <TableHead>
-            <TableRow>
-              {keys.map((key, index) => (
-                <TableCell key={index}>{key}</TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {/* Render a row for each value object */}
-            {values.map((valueObject, index) => (
-              <TableRow key={index}>
-                {/* Render a cell for each value in the value object */}
-                {keys.map((key, index) => (
-                  <TableCell key={index}>{valueObject[key]}</TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
+    if (response && response.type === "insight") {
+      if (
+        response.content ===
+        "Sorry, I am not able to handle the query, please use filter or reach out assistant"
+      ) {
+        return (
+          <div>
+            {submitted && (
+              <ListItem>
+                <ListItemIcon>
+                  <Avatar />
+                </ListItemIcon>
+                <span>{question}</span>
+              </ListItem>
+            )}
+            <p>{response.content}</p>
+          </div>
+        );
+      } else {
+        const content = JSON.parse(response.content);
+        const keys = Object.keys(content);
+        const values = Object.values(content);
+
+        return (
+          <div>
+            {submitted && (
+              <ListItem>
+                <ListItemIcon>
+                  <Avatar />
+                </ListItemIcon>
+                <span>{question}</span>
+              </ListItem>
+            )}
+            <TableContainer component={Paper}>
+              <Table aria-label="Insights table">
+                <TableBody>
+                  {keys.map((key, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{key}</TableCell>
+                      <TableCell>
+                        <List>
+                          {Object.entries(values[index]).map(
+                            ([innerKey, innerValue]) => (
+                              <ListItem key={innerKey}>
+                                <ListItemIcon>
+                                  <Done />
+                                </ListItemIcon>
+                                {innerValue}
+                              </ListItem>
+                            )
+                          )}
+                        </List>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        );
+      }
+    }
   };
 
   return (
     <div>
-      <div>
-        <form className="chatbot-form" onSubmit={handleQuestionSubmit}>
-          <TextField
-            id="standard-search"
-            label="Enter Your Question Here"
-            onChange={handleQuestionChange}
-            type="text"
-            variant="standard"
-            maxLength={250}
-            autoFocus
-          />
-        </form>
-        {loading && <p>Loading...</p>}
-        {error && <p className="chatbot-response">{response}</p>}
-        {submitted && isEmpty && (
-          <p className="error-message">Please enter a question.</p>
-        )}
-        {console.log("Response:", response)}
-        {console.log("Response Content:", response && response.content)}
-        {console.log(
-          "Object Entries:",
-          response.content && Object.entries(response.content)
-        )}
-        {response && typeof response === "object" ? (
-          // If response is an object (assumed to be JSON), render a table or any appropriate component
-          <CustomTable data={response} />
-        ) : (
-          // If response is not an object (assumed to be text), render the text directly
-          <p className="chatbot-response">{response}</p>
-        )}
-      </div>
-      {/* Table component removed */}
+      {renderContent()}
+      <Box
+        component="form"
+        onSubmit={handleQuestionSubmit}
+        sx={{
+          position: "fixed",
+          bottom: 0,
+          width: "100%",
+          bgcolor: "background.paper",
+          p: 2,
+        }}
+      >
+        <TextField
+          id="standard-search"
+          label="Enter Your Question Here"
+          onChange={handleQuestionChange}
+          type="text"
+          variant="standard"
+          maxLength={250}
+          autoFocus
+        />
+      </Box>
+      {loading && <BeatLoader color="blue" />}
+      {error && <p>{error}</p>}
+      {submitted && isEmpty && <p>Please enter a question.</p>}
     </div>
   );
 };
+
 export default Chatbot;
